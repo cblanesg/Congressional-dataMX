14d6a06a2d1a93822b31f9257d133517e419c246
cblanesg <cam.blanes@gmail.com>
2020-10-10 23:46:12 -0500

    add policy estimations roll call votes

--- source/02-outcomes/01-policy_positioning/00-roll_call/01-dw_nominate.R
+++ source/02-outcomes/01-policy_positioning/00-roll_call/01-dw_nominate.R
@@ -2,7 +2,7 @@
 
 library(tidyverse)
 library(stargazer)
-library(tidyr)
+library(ggplot2)
 library(wnominate)
 
 setwd('~/cam.blanes Dropbox/Camila Blanes/Congressional-dataMX/data/01-collection_data/')
@@ -26,32 +26,46 @@ nrow(id_data)
 # 0.1 Load Data- Roll Call Votes
 ###############################
 
-votes <- read_csv('06-aggregated-data/roll_call_votes.csv') %>%
+votes <- read_csv('../02-outcomes/01-policy_positioning/00-roll_call/00-prep_data/roll_call_votes60.csv') %>%
   select(-c(X1)) %>%
-  mutate(voto_num = ifelse(voto == 'A favor', 'yea', 
-                           ifelse(voto == 'En contra', 'nay', 'other')))
-
-
-votes <- left_join(id_data_prop, votes, by = c('id_legislador' = 'id', 'legislatura')) %>%
-  select(legislatura, id_legislador, titulo_votacion,voto_num, party) %>%
-  filter(!is.na(titulo_votacion)) 
+  mutate(voto_num = ifelse(vote == 'A favor', '1', 
+                           ifelse(vote == 'En contra', '6', '9'))) %>%
+  select(id_legislador, partido, voto_num, bill)
 
 
 #############################
-# 1.0 Rollcall() object
+# 1.0 Ideal Point Estimations
 #############################
 
+estimation_dwnominate <- function(votes,legislatura){
+  matrix_roll_call <- votes %>%
+    spread(bill, value = voto_num) %>%
+    as.matrix()
+  LEGnames <- matrix_roll_call[, 1]
+  legData <- matrix(matrix_roll_call[, 2], length(matrix_roll_call[, 2]), 1)
+  colnames(legData) <- "party"
+  matrix_roll_call <- matrix_roll_call[, -c(1, 2)]
+  
+  rc <- rollcall(matrix_roll_call, 
+                 yea = c('1'), 
+                 nay = c('6'), 
+                 missing = c('9'), 
+                 notInLegis = 0,
+                 legis.names = LEGnames, legis.data = legData,
+                 desc = "Mexican Deputies Chamber"
+  )
+  result <- wnominate(rc, polarity = c(1, 1))
+  
+  df_results <- as.data.frame(as.list(result$legislators['coord1D']))
+  df_results$coord2D <- as.list(result$legislators['coord2D'])$coord2D
+  df_results$party <- as.list(result$legislators['party'])$party
+  
+  ggplot(data = df_results, mapping = aes(x = coord1D, y = coord2D, colour = party)) + 
+    geom_point() + ggsave(filename = paste0('~/cam.blanes Dropbox/Camila Blanes/Congressional-dataMX/data/02-outcomes/01-policy_positioning/00-roll_call/02-graphs/dw-nominate', legislatura, '.png'))
+  
+  save(result, file = paste0('~/cam.blanes Dropbox/Camila Blanes/Congressional-dataMX/data/02-outcomes/01-policy_positioning/00-roll_call/01-ideal_points/dw_nominate', legislatura, '.Rda'))
+  return(result)
+}
 
-
-votes60 <- votes %>%
-  filter(legislatura == 60) %>%
-  mutate(code_vote = ifelse(voto_num == 'yea', 1, 
-                            ifelse(voto_num == 'nay', 6, 9))) %>%
-  group_by(id_legislador) %>%
-  count() %>%
-  filter(n == 590 )
-  select(id_legislador, party, code_vote) %>%
-  spread(id_legislador, code_vote)
-
-length(unique(votes60$titulo_votacion))
+r <- estimation_dwnominate(votes = votes, '60')
 
